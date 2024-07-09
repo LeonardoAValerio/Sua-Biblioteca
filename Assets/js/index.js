452eba4b50c73689db202159dc7ff81976fcdbb2
LeonardoAValerio <leonardoaugustovalerio@gmail.com>
2024-07-09 00:22:53 -0300

    Add: script: updateBookcas

---
+++ Assets/js/index.js
@@ -0,0 +1,69 @@
+function Book(title = '', author = '', yearPub = '', state = '', gender = '', categorie = '', synopsis = '', review = ''){ //Função construtora dos livros
+    this.title = title;
+    this.author = author;
+    this.yearPub = yearPub;
+    this.state = state;
+    this.gender = gender;
+    this.categorie = categorie;
+    this.synopsis = synopsis;
+    this.review = review;
+}
+
+const randomPercent = (min = 0, max = 100) => Math.round(Math.random() * (max - min) + min) //Cri uma porcentagem aleátoria
+
+const createDivBook = () => { //Cria a Div que vai ser o Book
+    const newBook = document.createElement('div'); //Cria a div
+    newBook.setAttribute('class', 'book'); //Coloca a classe book
+    newBook.style.height = `${randomPercent(85, 95)}%`; //Gera uma porcentagem aleátoria entre 90 e 100
+    return newBook; 
+}
+
+const createTitle = () => { //Cria o elemento do título
+    const title = document.createElement('p'); //Cria o P que vai conter o título
+    title.setAttribute('class', 'title-book'); //Coloca a classe 'title-book'
+    return title;
+}
+
+const createBook = objBook =>{ //Crio todo o elemento do livro (Recebe um objeto livro)
+    const book = createDivBook(); //Pega o livro
+    const title = createTitle(); //Pega o titlo
+    title.innerText = objBook.title; //Coloca no titulo o texto do livro
+    book.appendChild(title); //Coloca o titulo dentro do livor
+    return book; 
+}
+
+const createDivShelf = () => { //Cria o elemento de uma prateleheira
+    const newShelf = document.createElement('div'); //Cria a div
+    newShelf.setAttribute('class', 'shelf'); //Coloca nela a classe 'shelf'
+    return newShelf;
+}
+
+const createShelf = shelfBooks => { //Cria todo o elemento da estante (Recebe um array de livros)
+    const bookcase = document.querySelector('.bookcase'); //Pega bookcase
+    const shelf = createDivShelf(); //Cria o elemento shelf
+    for(let book of shelfBooks){ //Faz um loop dentro do array de livro pegando o objeto de cada livro
+        const newBook = createBook(book); //Cria o livro (Envia o objeto do livro)
+        shelf.appendChild(newBook); //Coloca na shelf o novo livro
+    };
+    bookcase.appendChild(shelf); //No final coloca toda a nova prateleira na estante
+}
+
+const updateBookcase = books =>{ //Atualiza toda a bookcase (Recebendo um array de objetos(Livros))
+    const numShelves = Math.ceil(books.length / 8); //Cálcula quantas shelves vão ter
+    for(let i = 0; i <= numShelves; i++){ //Faz um loop com a quantidade de shelves
+        const shelfBooks = books.slice(i * 8, (i + 1) * 8); //Pega até 8 livros do totais de livros
+        createShelf(shelfBooks); //Cria uma nova estante com os livros pegados
+    }
+}
+
+const b1 = new Book('Círculo de Chuva');
+const b2 = new Book('Hellboy');
+const b3 = new Book('A historias da vovó amélia');
+const b4 = new Book('Bicho piruleta');
+const b5 = new Book('Corações de neve');
+const b6 = new Book('Isso aí');
+const b7 = new Book('Não sei mais');
+const b8 = new Book('Pra onde ir');
+
+const books = [b1, b2, b3, b4, b5, b1, b2, b3, b4, b5];
+updateBookcase(books);
